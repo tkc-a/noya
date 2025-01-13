d839bd16cd44f382a7fb8aec72a17872b9914b4b
tkc-a <takeshi.a.web@gmail.com>
2025-01-13 16:03:38 +0900

    docs: Docコメントの追加

--- functions.php
+++ functions.php
@@ -14,17 +14,28 @@ function my_theme_add_editor_styles() {
 }
 add_action( 'after_setup_theme', 'my_theme_add_editor_styles' );
 
+/**
+ * 絵文字に関するスクリプト、スタイルを削除
+ * 
+ */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 add_action( 'wp_enqueue_scripts', function() {
     wp_dequeue_style( 'wp-emoji-styles' );
 });
 
+/**
+ * ブロックの装飾選択肢を増やし、スタイルを読み込む
+ * register_block_style...ブロックに装飾を追加
+ * wp_enqueue_block_style.追加した装飾のスタイルを読み込む
+ *
+ */
+
 function custom_block_styles(): void {
-    wp_enqueue_block_style(
-        'core/image',
+    wp_enqueue_block_style( //定義したスタイルの読み込み
+        'core/image', //ブロックを指定
         array(
-            'handle' => 'image-border-circle',
-            'src'    => get_parent_theme_file_uri( '/assets/css/image-styles.css' )
+            'handle' => 'image-border-circle', //装飾名
+            'src'    => get_parent_theme_file_uri( '/assets/css/image-styles.css' ) //読み込み先
         )
     );
 
@@ -36,19 +47,19 @@ function custom_block_styles(): void {
         )
     );
 
-    register_block_style(
-        'core/image', // ブロック名
+    register_block_style( //ブロックに装飾を定義
+        'core/image', // ブロックを指定
         array(
-            'name'         => 'image-border-circle', // スタイル名
-            'label'        => '丸', // スタイルの表示名
+            'name'         => 'image-border-circle', // 装飾名
+            'label'        => '丸', // 装飾の表示名
         )
     );
 
     register_block_style(
         'core/query-pagination',
         array(
-            'name'         => 'custom1-style', // スタイル名
-            'label'        => '装飾1', // スタイルの表示名
+            'name'         => 'custom1-style', 
+            'label'        => '装飾1'
         )
     );
     
