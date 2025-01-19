187cbd79a71bb8782bbcfed6e06d9ee1bda5b092
tkc-a <takeshi.a.web@gmail.com>
2025-01-20 07:52:32 +0900

    update: 余白をclump()に変換

--- patterns/header.php
+++ patterns/header.php
@@ -7,13 +7,13 @@
  */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|space-md","bottom":"var:preset|spacing|space-md","left":"var:preset|spacing|space-xl","right":"var:preset|spacing|space-xl"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="header-inner wp-block-group" style="padding-top:var(--wp--preset--spacing--space-md);padding-right:var(--wp--preset--spacing--space-xl);padding-bottom:var(--wp--preset--spacing--space-md);padding-left:var(--wp--preset--spacing--space-xl)">
-        <!-- wp:site-logo {"width":56,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
-        <!-- wp:group -->
-        <div class="header-nav wp-block-group">
-            <!-- wp:navigation {"icon":"menu","layout":{"type":"flex","justifyContent":"space-between"}} /-->
-        </div>
-        <!-- /wp:group -->
+<!-- wp:group {"className":"header-inner","style":{"spacing":{"padding":{"top":"var:preset|spacing|space-md","bottom":"var:preset|spacing|space-md","left":"var:preset|spacing|space-xxl","right":"var:preset|spacing|space-xxl"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group header-inner" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--space-md);padding-right:var(--wp--preset--spacing--space-xxl);padding-bottom:var(--wp--preset--spacing--space-md);padding-left:var(--wp--preset--spacing--space-xxl)">
+    <!-- wp:site-logo {"width":56,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+    <!-- wp:group {"className":"header-nav"} -->
+    <div class="wp-block-group header-nav">
+        <!-- wp:navigation {"ref":4,"icon":"menu","layout":{"type":"flex","justifyContent":"space-between"}} /-->
+    </div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
